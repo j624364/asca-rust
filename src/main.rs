0ea67e4cf9a94058a5a264af01b278531012a9db
Girv98 <james.girven10@gmail.com>
2024-12-08 04:35:49 +0000

    Write to file

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,24 @@ use colored::Colorize;
 use std::{fs, io, path::PathBuf, process::exit};
 use clap::Parser;
 
+#[cfg(windows)]
+const LINE_ENDING : &str = "\r\n";
+#[cfg(not(windows))]
+const LINE_ENDING : &str = "\n";
+
+fn ask(question: &str) -> bool {
+    println!("{}",question);
+    loop {
+        let mut buf = String::new();
+        let _ = std::io::stdin().read_line(&mut buf);
+        match buf.chars().next() {
+            Some('y' | 'Y') => return true,
+            Some('n' | 'N') => return false,
+            _ => println!("yes/no only."),
+        }
+    }
+}
+
 fn get_dir_files(path: &str, extension: &str) -> Result<Vec<PathBuf>, io::Error> {
     Ok(fs::read_dir(path)?
         // Filter out entries which we couldn't read
@@ -45,7 +63,7 @@ fn validate_file_exists(maybe_path: Option<PathBuf>, extension: &str) -> Result<
     exit(1);
 }
 
-fn run_cli(rules: Option<PathBuf>, input: Option<PathBuf>, output: Option<PathBuf>) -> Result<(), io::Error> {
+fn cli(rules: Option<PathBuf>, input: Option<PathBuf>, output: Option<PathBuf>) -> Result<(), io::Error> {
 
     let rule_file_path = validate_file_exists(rules, "asca")?;
     let word_file_path = validate_file_exists(input, "txt")?;
@@ -53,7 +71,7 @@ fn run_cli(rules: Option<PathBuf>, input: Option<PathBuf>, output: Option<PathBu
     let word: Vec<String> = fs::read_to_string(word_file_path)?.lines().map(|s| s.to_owned()).collect();
     
 
-    let res = asca::run(rules, word.clone());
+    let res = asca::run_cli(&rules, &word);
 
     if res.len() == 1 && res[0].contains("Error") {
         println!("{}", res[0])
@@ -67,8 +85,34 @@ fn run_cli(rules: Option<PathBuf>, input: Option<PathBuf>, output: Option<PathBu
         }
     }
 
+    println!();
+
     if let Some(out_path) = output {
-        // validate path and then write to file
+        if out_path.is_file() {
+            // if path is an extant file, overwrite on accept
+            if ask(&(format!("File {out_path:?} already exists, do you wish to overwrite it? [y/n]"))) {
+                fs::write(&out_path, res.join(LINE_ENDING)).expect("Unable to write file");
+                println!("Written to file {:?}", out_path);
+            }
+        } else if out_path.is_dir() {
+            // if path is dir, write to file of <dir>/out.txt
+            let p = PathBuf::from("out.txt");
+            if p.exists() {
+                if ask(&(format!("File {p:?} already exists, do you wish to overwrite it? [y/n]"))) {
+                    fs::write(&out_path, res.join(LINE_ENDING)).expect("Unable to write file");
+                    println!("Written to file {:?}", out_path);
+                }
+            } else {
+                fs::write(&p, res.join(LINE_ENDING)).expect("Unable to write file");
+                println!("Written to file 'out.txt'");
+            }
+        } else if out_path.extension().map_or(false, |ext| ext == "txt") {
+            // create file <out_path> and write to it
+            fs::write(&out_path, res.join(LINE_ENDING)).expect("Unable to write file");
+            println!("Written to file {:?}", out_path);
+        } else {
+            // Wrong file type?
+        }
     }
 
     Ok(())
@@ -76,20 +120,13 @@ fn run_cli(rules: Option<PathBuf>, input: Option<PathBuf>, output: Option<PathBu
 
 fn main() {
     let args = CliArgs::parse();
-
     match args.cmd {
         Command::Run { rules, input, output } => {
-            if let Err(e) = run_cli(rules, input, output) {
+            if let Err(e) = cli(rules, input, output) {
                 println!("{e}");
                 exit(1);
             }
         },
-        Command::Tui => todo!(),
+        Command::Tui => todo!("tui coming soon"),
     }
-
-    // let res = asca::run(unparsed_rules, unparsed_words);
-
-    // for r in res {
-    //     println!("{r}");
-    // }
 }
